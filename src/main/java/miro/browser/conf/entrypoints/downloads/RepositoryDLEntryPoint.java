c14ede8b0c65ccae19eb1b8ffc7c17c6fc8f0b94
Andreas Reuter <andreas.router@gmail.com>
2015-10-14 17:31:48 +0200

    Add simple download API for ResourceCertificateTree objects

--- src/main/java/miro/browser/conf/entrypoints/downloads/RepositoryDLEntryPoint.java
+++ src/main/java/miro/browser/conf/entrypoints/downloads/RepositoryDLEntryPoint.java
@@ -1,6 +1,11 @@
 package main.java.miro.browser.conf.entrypoints.downloads;
 
+import java.io.File;
+
 import javax.servlet.http.HttpServletRequest;
+
+import main.java.miro.browser.browser.updater.ModelUpdater;
+import main.java.miro.browser.util.DownloadHandler;
 
 import org.eclipse.core.databinding.observable.Realm;
 import org.eclipse.jface.databinding.swt.SWTObservables;
@@ -20,9 +25,34 @@ public class RepositoryDLEntryPoint extends AbstractEntryPoint {
 		@Override
 		public void run() {
 			HttpServletRequest request = RWT.getRequest();
-			String foo = request.getParameter( "foo" );
-			// TODO Auto-generated method stub
-			
+			String key = request.getParameter("key");
+			String filepath = getModelFilepath(key);
+			if(isFail(filepath))
+				handleNullCase();
+			else {
+				DownloadHandler dlHandler = new DownloadHandler();
+				dlHandler.sendDownload(filepath);
+			}
+		}
+
+		private String getModelFilepath(String key) {
+			if(key == null)
+				return "";
+			return ModelUpdater.EXPORT_DIRECTORY + key;
+		}
+
+		private boolean isFail(String filepath) {
+			if(filepath == "")
+				return true;
+			if(!(new File(filepath)).exists()){
+				return true;
+			}
+			return false;
+		}
+
+		private void handleNullCase() {
+			//TODO figure out how to handle 404 case. Probably have to look into DownloadHandler and ClientServices in general to find a 
+			//good solution
 		}
 	}
 }
